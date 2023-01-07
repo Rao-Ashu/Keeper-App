56a1826608d0a82f83193a014abef0b10e4dc84b
Rao-Ashu <mit2021066@iiita.ac.in>
2023-01-07 11:48:08 +0530

    deploying

---
+++ build/precache-manifest.a94dc718e0e052bbe5db8385a9b403d0.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7c809a3fb7277dd772f37a440ee337b",
+    "url": "/Rao-Ashu/keeper-app/index.html"
+  },
+  {
+    "revision": "28590cc89be37effedf3",
+    "url": "/Rao-Ashu/keeper-app/static/js/2.e191c4e3.chunk.js"
+  },
+  {
+    "revision": "dc0b378dc77b25c62a0c",
+    "url": "/Rao-Ashu/keeper-app/static/js/main.3c9370f1.chunk.js"
+  },
+  {
+    "revision": "25a001c796845719779f",
+    "url": "/Rao-Ashu/keeper-app/static/js/runtime-main.f1839091.js"
+  }
+]);
